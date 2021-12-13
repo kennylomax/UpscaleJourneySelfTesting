39cf7d872cefbf835d07cfe27e5770394f525712
kennylomax <>
2021-12-13 13:10:29 +0100

    env

--- journeymaterial/exampleupscaleenv.sh
+++ journeymaterial/exampleupscaleenv.sh
@@ -1,9 +1,9 @@
-# Personlize these to your environment
+# Personalize these to your environment
 export MY_UPSCALE_WORKBENCH=https://xxx.approuter.stage-use1.js-stage.shoot.live.k8s-hana.ondemand.com 
 export MY_UPSCALE_EMAIL=xxx.xxx@sap.com 
 export MY_UPSCALE_PASSWORD=xxx 
 export MY_GITHUB_USERNAME=xxx
-# Needs repo and admin:plublic_key rights
+# Create a Github token with repo and admin:plublic_key rights (see https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token)
 export MY_GITHUB_TOKEN=xxx
 export MY_GITHUB_EMAIL=xxx
 export MY_DOWNLOAD_FOLDER=/Users/xxx/Downloads  
