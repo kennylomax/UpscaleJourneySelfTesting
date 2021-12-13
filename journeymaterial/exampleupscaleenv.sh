4c02b5bb270e5ce06a9af1176ac5ec134a7602d4
kennylomax <>
2021-12-13 13:03:19 +0100

    env

--- journeymaterial/exampleupscaleenv.sh
+++ journeymaterial/exampleupscaleenv.sh
@@ -6,15 +6,8 @@ export MY_GITHUB_USERNAME=xxx
 # Needs repo and admin:plublic_key rights
 export MY_GITHUB_TOKEN=xxx
 export MY_GITHUB_EMAIL=xxx
-
-# On OSX..
 export MY_DOWNLOAD_FOLDER=/Users/xxx/Downloads  
 export MY_HOME_DIRECTORY_PREFIX=Absolute folder location where this journey should occur
-
-
-# On Linux for Docker
-# export MY_DOWNLOAD_FOLDER=/home/chrome/Downloads
-# export MY_HOME_DIRECTORY_PREFIX=/src/journey
 
 #Leave these alone
 export MY_HOME_DIRECTORY=${MY_HOME_DIRECTORY_PREFIX}/${NOW}
@@ -28,6 +21,8 @@ if [[ "$OSTYPE" == *"darwin"* ]]; then
   ng analytics off
 else 
   # Linux for Docker
+  export MY_DOWNLOAD_FOLDER=/home/chrome/Downloads
+  export MY_HOME_DIRECTORY_PREFIX=/src/journey
   apt-get update    
   # See https://linuxize.com/post/how-to-install-node-js-on-debian-10/
   curl -sL https://deb.nodesource.com/setup_12.x |  bash -
